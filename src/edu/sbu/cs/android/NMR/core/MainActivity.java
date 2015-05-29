3612307b2b0388c76c1602f27edd4c33d6333fd7
zlotniam12 <zlotniam12@bonaventure.edu>
2015-05-29 14:51:11 -0400

    Update MainActivity.java
    Changed to allow for more fragments, communication between fragments

--- src/edu/sbu/cs/android/NMR/core/MainActivity.java
+++ src/edu/sbu/cs/android/NMR/core/MainActivity.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import edu.sbu.cs.android.NMR.adapter.TabsPagerAdapter;
+import edu.sbu.cs.android.NMR.core.HomeFragment.OnItemSelectedListener;
 import edu.sbu.cs.android.R;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
@@ -21,18 +22,19 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends FragmentActivity implements
-	ActionBar.TabListener{
+public class MainActivity extends FragmentActivity implements  OnItemSelectedListener,
+ActionBar.TabListener{
 	Bundle bd;
 	private NonSwipeableViewPager viewPager;
 	//DrawingView dv=new DrawingView(this,);
 	private TabsPagerAdapter mAdapter;
 	private ActionBar actionBar;
 	// Tab titles
-	private String[] tabs = { "Spectra", "Questions", "SolveIt" };
+	private String[] tabs = { "Home", "Spectra", "Questions", "SolveIt" };
 	private boolean lock=false;
 	File file;
 	String path;
+	private int problem = 1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -76,76 +78,75 @@ public class MainActivity extends FragmentActivity implements
 		});
 	}
 
-	public void writeToString(File file,String str){
-		FileOutputStream stream = null;
-		try {
-			stream = new FileOutputStream(file);
-			stream.write(str.getBytes());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally {
-			try {
-				stream.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public String loadJSONFromAsset() {
-		String json = null;
-		try {
-
-			InputStream is = getAssets().open("peak.json");
-			
-			//InputStream is= getApplicationContext().getResources().openRawResource(R.raw.peak);
-			int size = is.available();
-
-			byte[] buffer = new byte[size];
-
-			is.read(buffer);
-
-			is.close();
-
-			json = new String(buffer, "UTF-8");
-
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			return null;
-		}
-		return json;
-
-	}
+//	public void writeToString(File file,String str){
+//		FileOutputStream stream = null;
+//		try {
+//			stream = new FileOutputStream(file);
+//			stream.write(str.getBytes());
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		finally {
+//			try {
+//				stream.close();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+//
+//	public String loadJSONFromAsset() {
+//		String json = null;
+//		try {
+//
+//			InputStream is = getAssets().open("peak.json");
+//			
+//			//InputStream is= getApplicationContext().getResources().openRawResource(R.raw.peak);
+//			int size = is.available();
+//
+//			byte[] buffer = new byte[size];
+//
+//			is.read(buffer);
+//
+//			is.close();
+//
+//			json = new String(buffer, "UTF-8");
+//
+//
+//		} catch (IOException ex) {
+//			ex.printStackTrace();
+//			return null;
+//		}
+//		return json;
+//
+//	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    // Handle item selection
 	    switch (item.getItemId()) {
 	    case R.id.action_NMR:
-
-	    	HomeFragment.w.loadUrl("file:///android_asset/nmr.html");
-	    	HomeFragment.w.getSettings().setBuiltInZoomControls(true);
-	    	HomeFragment.w.getSettings().setDisplayZoomControls(false);
+	    	SpectraFragment.w.loadUrl("file:///android_asset/nmr.html");
+	    	SpectraFragment.w.getSettings().setBuiltInZoomControls(true);
+	    	SpectraFragment.w.getSettings().setDisplayZoomControls(false);
 	       return true;
 	    case R.id.action_IR:
-	    	HomeFragment.w.loadUrl("file:///android_asset/ir.html");
-	    	HomeFragment.w.getSettings().setBuiltInZoomControls(true);
-	    	HomeFragment.w.getSettings().setDisplayZoomControls(false);
+	    	SpectraFragment.w.loadUrl("file:///android_asset/ir.html");
+	    	SpectraFragment.w.getSettings().setBuiltInZoomControls(true);
+	    	SpectraFragment.w.getSettings().setDisplayZoomControls(false);
 	        return true;
 	    case R.id.action_CNMR:
-	    	HomeFragment.w.loadUrl("file:///android_asset/cnmr.html");
-	    	HomeFragment.w.getSettings().setBuiltInZoomControls(true);
-	    	HomeFragment.w.getSettings().setDisplayZoomControls(false);
+	    	SpectraFragment.w.loadUrl("file:///android_asset/cnmr.html");
+	    	SpectraFragment.w.getSettings().setBuiltInZoomControls(true);
+	    	SpectraFragment.w.getSettings().setDisplayZoomControls(false);
 	        return true;
 	    case R.id.action_Reset:
-//	    	writeToString(file,loadJSONFromAsset());
-//	    	Toast.makeText(getApplication(),"reset act",Toast.LENGTH_SHORT).show();
+	    	//writeToString(file,loadJSONFromAsset());
+	    	//Toast.makeText(getApplication(),"reset act",Toast.LENGTH_SHORT).show();
 	        return true;
 	    case R.id.action_Lock:
 	    	lock=!lock;
@@ -160,6 +161,42 @@ public class MainActivity extends FragmentActivity implements
 	        return super.onOptionsItemSelected(item);
 	    }
 	}
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item2) {
+//	    // Handle item selection
+//	    switch (item.getItemId()) {
+//	    case R.id.action_SPECTRUM1:
+//	    	HomeFragment.w.loadUrl("file:///android_asset/nmr.html");
+//	    	HomeFragment.w.getSettings().setBuiltInZoomControls(true);
+//	    	HomeFragment.w.getSettings().setDisplayZoomControls(false);
+//	       return true;
+//	    case R.id.action_IR:
+//	    	HomeFragment.w.loadUrl("file:///android_asset/ir.html");
+//	    	HomeFragment.w.getSettings().setBuiltInZoomControls(true);
+//	    	HomeFragment.w.getSettings().setDisplayZoomControls(false);
+//	        return true;
+//	    case R.id.action_CNMR:
+//	    	HomeFragment.w.loadUrl("file:///android_asset/cnmr.html");
+//	    	HomeFragment.w.getSettings().setBuiltInZoomControls(true);
+//	    	HomeFragment.w.getSettings().setDisplayZoomControls(false);
+//	        return true;
+//	    case R.id.action_Reset:
+//	    	//writeToString(file,loadJSONFromAsset());
+//	    	//Toast.makeText(getApplication(),"reset act",Toast.LENGTH_SHORT).show();
+//	        return true;
+//	    case R.id.action_Lock:
+//	    	lock=!lock;
+//	    	if(lock)
+//	    	actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+//	    	else
+//	    		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+//	    	
+//	    	viewPager.setBlockSwipe(lock);
+//	        return true;
+//	    default:
+//	        return super.onOptionsItemSelected(item);
+//	    }
+//	}
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 	}
@@ -187,5 +224,17 @@ public class MainActivity extends FragmentActivity implements
 	    moveTaskToBack(true);
 		//wv.loadUrl("file:///android_asset/index.html");
 	}
-	
+	public int getProblem(){
+	    return this.problem;
+	}
+
+	public void setProblem(int num){
+	    this.problem = num;
+	}
+
+	@Override
+	public void onItemSelected() {
+		// TODO Auto-generated method stub
+		
+	}
 }
